776e20d3ce5e084ae5b85ef6e29c7513d516324e
zakirya592 <muhammadzakirya592@gmail.com>
2025-01-23 00:44:47 +0500

    updated

--- frontend/src/Pages/patientInformation/patientTable.jsx
+++ frontend/src/Pages/patientInformation/patientTable.jsx
@@ -84,7 +84,7 @@ function PatientTable() {
       <div className="font-[sans-serif] ">
         <SideNav>
           <div className="w-11/12 mx-auto">
-            <div className="flex items-center mb-4 justify-between mt-16 w-full">
+            <div className="flex items-center mb-4 justify-between w-full">
               <h3 className="text-2xl font-bold text-left mt-10">
                 Patients Informaation
               </h3>
@@ -105,143 +105,144 @@ function PatientTable() {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <table className="min-w-full bg-white overflow-x-auto">
-              <thead className="whitespace-nowrap">
-                <tr>
-                  <th className="pl-4 w-8"> {/* Checkbox Header */} </th>
-                  <th className="p-4 text-left text-sm font-semibold text-gray-800">
-                    Name
-                  </th>
-                  <th className="p-4 text-left text-sm font-semibold text-gray-800">
-                    Status
-                  </th>
-                  <th className="p-4 text-left text-sm font-semibold text-gray-800">
-                    Age
-                  </th>
-                  <th className="p-4 text-left text-sm font-semibold text-gray-800">
-                    Contact
-                  </th>
-                  <th className="p-4 text-center text-sm font-semibold text-gray-800">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="whitespace-nowrap">
-                {loading ? (
-                  <Spinner />
-                ) : (
-                  patients
-                    .filter(
-                      (patient) =>
-                        patient.name
-                          .toLowerCase()
-                          .includes(searchTerm.toLowerCase()) ||
-                        patient.status
-                          .toLowerCase()
-                          .includes(searchTerm.toLowerCase()) ||
-                        patient.age.toString().includes(searchTerm) ||
-                        patient.mobileNumber.includes(searchTerm)
-                    )
-                    .map((patient) => (
-                      <tr
-                        key={patient.id}
-                        className={`odd:bg-blue-50 ${selectedPatientId === patient.id ? "bg-white" : ""
-                          }`}
-                        onClick={() => setSelectedPatientId(patient.id)}
-                      >
-                        <td className="pl-4 w-8">
-                          <input
-                            id={`checkbox-${patient.id}`}
-                            type="checkbox"
-                            className="hidden peer"
-                          />
-                          <label
-                            htmlFor={`checkbox-${patient.id}`}
-                            className="relative flex items-center justify-center p-0.5 peer-checked:before:hidden before:block before:absolute before-w-full before-h-full before:bg-white w-5 h-5 cursor-pointer bg-white-500 border border-gray-400 rounded overflow-hidden"
-                          >
-                            {selectedPatientId === patient.id && (
-                              <span>✔️</span>
+            <div className="max-h-[500px] sm:max-h-[300px] md:max-h-[450px] lg:max-h-[500px] overflow-y-scroll border border-gray-300 rounded-md mt-4">
+              <table className="min-w-full bg-white  overflow-y-scroll">
+                <thead className="whitespace-nowrap">
+                  <tr>
+                    <th className="pl-4 w-8"> {/* Checkbox Header */} </th>
+                    <th className="p-4 text-left text-sm font-semibold text-gray-800">
+                      Name
+                    </th>
+                    <th className="p-4 text-left text-sm font-semibold text-gray-800">
+                      Status
+                    </th>
+                    <th className="p-4 text-left text-sm font-semibold text-gray-800">
+                      Age
+                    </th>
+                    <th className="p-4 text-left text-sm font-semibold text-gray-800">
+                      Contact
+                    </th>
+                    <th className="p-4 text-center text-sm font-semibold text-gray-800">
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="whitespace-nowrap">
+                  {loading ? (
+                    <Spinner />
+                  ) : (
+                    patients
+                      .filter(
+                        (patient) =>
+                          patient.name
+                            .toLowerCase()
+                            .includes(searchTerm.toLowerCase()) ||
+                          patient.status
+                            .toLowerCase()
+                            .includes(searchTerm.toLowerCase()) ||
+                          patient.age.toString().includes(searchTerm) ||
+                          patient.mobileNumber.includes(searchTerm)
+                      )
+                      .map((patient) => (
+                        <tr
+                          key={patient.id}
+                          className={`odd:bg-blue-50 ${selectedPatientId === patient.id ? "bg-white" : ""
+                            }`}
+                          onClick={() => setSelectedPatientId(patient.id)}
+                        >
+                          <td className="pl-4 w-8">
+                            <input
+                              id={`checkbox-${patient.id}`}
+                              type="checkbox"
+                              className="hidden peer"
+                            />
+                            <label
+                              htmlFor={`checkbox-${patient.id}`}
+                              className="relative flex items-center justify-center p-0.5 peer-checked:before:hidden before:block before:absolute before-w-full before-h-full before:bg-white w-5 h-5 cursor-pointer bg-white-500 border border-gray-400 rounded overflow-hidden"
+                            >
+                              {selectedPatientId === patient.id && (
+                                <span>✔️</span>
+                              )}
+                            </label>
+                          </td>
+                          <td className="p-4 text-sm text-gray-800">
+                            {patient.name}
+                          </td>
+                          <td className="p-4 text-sm text-gray-800">
+                            <span
+                              className={`w-[68px] block text-center py-1 border ${patient.status === "Active"
+                                  ? "border-green-500 text-green-600"
+                                  : "border-yellow-500 text-yellow-600"
+                                } rounded text-xs`}
+                            >
+                              {patient.status}
+                            </span>
+                          </td>
+                          <td className="p-4 text-sm text-gray-800">
+                            {patient.age}
+                          </td>
+                          <td className="p-4 text-sm text-gray-800">
+                            {patient.mobileNumber}
+                          </td>
+                          <td className="relative text-center">
+                            <button
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                toggleDropdown(patient.id);
+                              }}
+                              className="text-gray-500 text-lg hover:text-blue-600"
+                            >
+                              ⋮
+                            </button>
+                            {dropdownVisible === patient.id && (
+                              <div className="absolute bg-white border rounded shadow-lg w-40 z-10">
+                                <ul>
+                                  <li
+                                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer flex my-auto"
+                                    onClick={() => handleupdated(patient)}
+                                  >
+                                    <CiEdit className="my-auto me-4" />{" "}
+                                    <p className="text-lg ">Edit</p>
+                                  </li>
+                                  <li
+                                    className="px-4 py-2 hover:bg-gray-100 text-red-600 cursor-pointer flex my-auto"
+                                    onClick={() => handleDelete(patient)}
+                                  >
+                                    <FaTrash className="my-auto me-4" />{" "}
+                                    <p className="text-lg ">Delete</p>
+                                  </li>
+                                </ul>
+                              </div>
                             )}
-                          </label>
-                        </td>
-                        <td className="p-4 text-sm text-gray-800">
-                          {patient.name}
-                        </td>
-                        <td className="p-4 text-sm text-gray-800">
-                          <span
-                            className={`w-[68px] block text-center py-1 border ${patient.status === "Active"
-                                ? "border-green-500 text-green-600"
-                                : "border-yellow-500 text-yellow-600"
-                              } rounded text-xs`}
-                          >
-                            {patient.status}
-                          </span>
-                        </td>
-                        <td className="p-4 text-sm text-gray-800">
-                          {patient.age}
-                        </td>
-                        <td className="p-4 text-sm text-gray-800">
-                          {patient.mobileNumber}
-                        </td>
-                        <td className="relative text-center">
-                          <button
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              toggleDropdown(patient.id);
-                            }}
-                            className="text-gray-500 text-lg hover:text-blue-600"
-                          >
-                            ⋮
-                          </button>
-                          {dropdownVisible === patient.id && (
-                            <div className="absolute bg-white border rounded shadow-lg w-40 z-10">
-                              <ul>
-                                <li
-                                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer flex my-auto"
-                                  onClick={() => handleupdated(patient)}
-                                >
-                                  <CiEdit className="my-auto me-4" />{" "}
-                                  <p className="text-lg ">Edit</p>
-                                </li>
-                                <li
-                                  className="px-4 py-2 hover:bg-gray-100 text-red-600 cursor-pointer flex my-auto"
-                                  onClick={() => handleDelete(patient)}
-                                >
-                                  <FaTrash className="my-auto me-4" />{" "}
-                                  <p className="text-lg ">Delete</p>
-                                </li>
-                              </ul>
-                            </div>
-                          )}
-                        </td>
-                      </tr>
-                    ))
-                )}
-              </tbody>
-            </table>
+                          </td>
+                        </tr>
+                      ))
+                  )}
+                </tbody>
+              </table>
+            </div>
+            <div className="flex justify-end mb-4 mt-6">
+              <button
+                onClick={handlePreviousPage}
+                className={`px-4 py-2 bg-green-500 text-white rounded mr-2 ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
+                  }`}
+                disabled={currentPage === 1}
+              >
+                Previous Page
+              </button>
+              <button
+                onClick={handleNextPage}
+                className={`px-4 py-2 bg-green-500 text-white rounded ${currentPage === totalPages
+                    ? "opacity-50 cursor-not-allowed"
+                    : ""
+                  }`}
+                disabled={currentPage === totalPages}
+              >
+                Next Page
+              </button>
+            </div>
           </div>
         </SideNav>
-      </div>
-      <div
-        className="flex justify-center mt-4 mb-4"
-        style={{ marginLeft: "1150px" }}
-      >
-        <button
-          onClick={handlePreviousPage}
-          className={`px-4 py-2 bg-green-500 text-white rounded mr-2 ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
-            }`}
-          disabled={currentPage === 1}
-        >
-          Previous Page
-        </button>
-        <button
-          onClick={handleNextPage}
-          className={`px-4 py-2 bg-green-500 text-white rounded ${currentPage === totalPages ? "opacity-50 cursor-not-allowed" : ""
-            }`}
-          disabled={currentPage === totalPages}
-        >
-          Next Page
-        </button>
       </div>
     </>
   );
